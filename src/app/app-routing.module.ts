a42416cdafd64b2db5b16960c9ed060b4a74b944
Hannes Kimara <hanneskimara@gmail.com>
2020-04-03 14:10:07 +0300

    Implement Registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,8 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {path:'', component: LandingComponent},
   {path:'home', redirectTo: ''},
+  {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent}
-
 ];
 
 @NgModule({
